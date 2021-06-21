fbfefc7794be2619d7a86929f0ffe192ff5a4097
amoschoomy <acho0057@student.monash.edu>
2021-06-21 13:57:11 +0800

    Added blank fragments to the activity

--- app/src/main/java/com/amoschoojs/livinginchrist/MainActivity.kt
+++ app/src/main/java/com/amoschoojs/livinginchrist/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.amoschoojs.livinginchrist
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ScrollView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import androidx.cardview.widget.CardView
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainDrawer : DrawerLayout
     private  var toggleStatus: Boolean = false
-    private lateinit var nightModeToggle: SwitchCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nav_drawer)
@@ -31,6 +32,29 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
         val navigationView = findViewById<NavigationView>(R.id.navview)
         navigationView.setNavigationItemSelectedListener(CustomNavigationViewListener(this))
+        cardViewListener()
+    }
+
+    private fun cardViewListener() {
+       val counterCard: CardView = findViewById(R.id.countercard)
+        val planCard: CardView = findViewById(R.id.plancard)
+        val quizCard :CardView = findViewById(R.id.quizcard)
+        val studyCard : CardView = findViewById(R.id.studycard)
+        val fragmentManager: FragmentManager= supportFragmentManager
+        counterCard.setOnClickListener{
+            fragmentManager.beginTransaction().replace(R.id.frag1,BlankFragment()).addToBackStack("counter").commit()
+        }
+        planCard.setOnClickListener{
+            fragmentManager.beginTransaction().replace(R.id.frag1,PlanFragment()).addToBackStack("plans").commit()
+        }
+        quizCard.setOnClickListener{
+            fragmentManager.beginTransaction().replace(R.id.frag1,QuizFragment()).addToBackStack("quizzes").commit()
+        }
+        studyCard.setOnClickListener{
+            fragmentManager.beginTransaction().replace(R.id.frag1,BibleStudyFragment()).addToBackStack("bibstudy").commit()
+        }
+
+
 
     }
 
