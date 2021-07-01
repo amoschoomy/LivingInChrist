c0516f35dab06dd09bd257cae2d7d01ae27507ec
amoschoomy <acho0057@student.monash.edu>
2021-07-01 14:59:26 +0800

    Updated array to include all books

--- app/src/main/java/com/amoschoojs/livinginchrist/BookStudy.kt
+++ app/src/main/java/com/amoschoojs/livinginchrist/BookStudy.kt
@@ -16,7 +16,14 @@ class BookStudy : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_study)
 
-        books= arrayOf("Genesis","Exodus","Numbers","Leviticus","Deutoronomy")
+        books= arrayOf("Genesis","Exodus","Numbers","Leviticus","Deuteronomy","Joshua","Ruth",
+        "1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah"
+        ,"Nehemiah","Esther","Job","Psalm","Proverbs","Ecclesiastes","Song of Songs","Isaiah",
+        "Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah"
+        ,"Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew","Mark","Luke"
+        ,"John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians",
+        "Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon",
+        "Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelations")
         val bookRecyclerViewAdapter=BooksRecyclerViewAdapter(books)
         val bookrecview=findViewById<RecyclerView>(R.id.bookrecview)
         val linearLayoutManager=LinearLayoutManager(this)
